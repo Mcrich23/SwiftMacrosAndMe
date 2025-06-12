3e200b49a6cb8bb8fbf5436d729b2ca2357f32b4
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-06-11 21:53:33 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import CompilerPluginSupport
 
 let package = Package(
     name: "SwiftMacrosAndMe",
-    platforms: [.macOS(.v14), .iOS(.v17), .tvOS(.v17), .watchOS(.v9), .macCatalyst(.v14)],
+    platforms: [.macOS(.v14), .iOS(.v17), .tvOS(.v17), .watchOS(.v9)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
