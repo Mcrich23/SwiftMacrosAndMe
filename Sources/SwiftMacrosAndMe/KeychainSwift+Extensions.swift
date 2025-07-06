fe928fc55081004fca1453732a4b551341bdb403
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-07-05 21:59:57 -0700

    Update KeychainSwift+Extensions.swift

--- Sources/SwiftMacrosAndMe/KeychainSwift+Extensions.swift
+++ Sources/SwiftMacrosAndMe/KeychainSwift+Extensions.swift
@@ -5,7 +5,7 @@
 //  Created by Morris Richman on 6/8/25.
 //
 
-@preconcurrency import KeychainSwift
+@preconcurrency public import KeychainSwift
 
 public extension KeychainSwift {
     static let shared = KeychainSwift()
